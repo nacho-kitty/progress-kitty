b0818a55800706e565150a5dd85b444e8f9745b3
funneralplanner <plsdontatm3@gmail.com>
2025-09-21 16:35:53 -0400

    added 4 columns, added cards, added f string

--- basic-app/app.py
+++ basic-app/app.py
@@ -1,10 +1,31 @@
 from shiny import render, ui
-from shiny.express import input
+from shiny.express import *
 
-ui.panel_title("Hello Shiny!")
-ui.input_slider("n", "N", 0, 100, 20)
+ui.panel_title("Progress-kitty!")
 
 
-@render.text
-def txt():
-    return f"n*2 is {input.n() * 2}"
+with ui.layout_columns(col_widths= [3,3,3,3]):
+    with ui.card():
+        "task 1"
+        ui.input_slider("n1", "N", 0, 100, 0)
+        @render.text
+        def txt1():
+            return f"this task is about {input.n1()}% done"
+    with ui.card():
+        "card 2"
+        ui.input_slider("n2", "N", 0, 100, 0)
+        @render.text
+        def txt2():
+            return f"this task is about {input.n2()}% done"
+    with ui.card():
+        "card 3"
+        ui.input_slider("n3", "N", 0, 100, 0)
+        @render.text
+        def txt3():
+            return f"this task is about {input.n3()}% done"
+    with ui.card():
+        "card 4"
+        ui.input_slider("n4", "N", 0, 100, 0)
+        @render.text
+        def txt4():
+            return f"this task is about {input.n4()}% done"
